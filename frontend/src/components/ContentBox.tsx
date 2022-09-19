c219e69767b08782e100c502ae6debc817e056d9
Beate Simonsen <bmsimons@stud.ntnu.no>
2022-09-19 14:44:53 +0200

    (#24) Replace Stack with Table for trending words

--- frontend/src/components/ContentBox.tsx
+++ frontend/src/components/ContentBox.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Flex, Center, HStack, VStack, Text, Spacer, Divider } from "@chakra-ui/react";
+import { Center, Table, TableContainer, Thead, Tr, Th, Td, Tbody } from "@chakra-ui/react";
 
 interface Item {
     name: string;
@@ -15,35 +15,23 @@ interface Props {
 const ContentBox: React.FC<Props> = ({ category, statName, items }: Props) => {
 	return (
 		<Center>
-			<Flex bg="mediumGrey">
-				<VStack w="70vw">
-					<HStack p="50px 300px 5px 50px" w="100%">
-						<Text fontSize="2xl">{category}</Text>
-						<Spacer/>
-						<Text fontSize="2xl">{statName}</Text>
-					</HStack>
-					<Divider borderColor={"black"} w="90%"/>
-					<HStack p="5px 320px 5px 50px" w="100%">
-						<VStack align="left">
-							{items.map((item, index) => {
-								return (<Text key={index} fontSize="3xl">
-									{`${index + 1}. ${item.name}`}
-								</Text>);
-							}
-							)};
-						</VStack>
-						<Spacer/>
-						<VStack align="left">
-							{items.map((item, index) => {
-								return (<Text key={index} fontSize="3xl">
-									{` ${item.value}`}
-								</Text>);
-							}
-							)};
-						</VStack>
-					</HStack>
-				</VStack>
-			</Flex>
+			<TableContainer w="60vw" p="20px" bg="mediumGrey">
+				<Table variant='unstyled'>
+					<Thead>
+						<Tr borderBottom="2px" w="90%">
+							<Th textTransform="lowercase" fontSize="2xl">{category}</Th>
+							<Th textTransform="lowercase" fontSize="2xl">{statName}</Th>
+						</Tr>
+					</Thead>
+					<Tbody>
+						{items.map((item, index) => {
+							return (<Tr key={index}>
+								<Td fontSize="3xl" p="20px">{`${index + 1}. ${item.name}`}</Td>
+								<Td fontSize="3xl" p="20px">{`${item.value}`}</Td>
+							</Tr>);})}
+					</Tbody>
+				</Table>
+			</TableContainer>
 		</Center>
 	);
 };
