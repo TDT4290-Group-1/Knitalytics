444d54260eb7f9df2549badafe8c8d319a04878c
Victoria Hopso <victoria.hopso@gmail.com>
2022-09-28 11:50:55 +0200

    Adjusts styling of homepage

--- frontend/src/components/ContentBox.tsx
+++ frontend/src/components/ContentBox.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Center, Table, TableContainer, Thead, Tr, Th, Td, Tbody, Switch, HStack } from "@chakra-ui/react";
 import { TiSortNumerically } from "react-icons/ti";
 import { BsPercent } from "react-icons/bs";
-import theme from "theme";
+import theme from "../theme";
 import { TrendingWord } from "../../models/trendingword";
 
 
@@ -14,40 +14,40 @@ interface Props {
 
 const ContentBox: React.FC<Props> = ({ category, statName, items }: Props) => {
 
-	console.log("HJEHJEH", items);
-	const [toggle, setToggle] = useState(false);
+	const [toggle, setToggle] = useState(true);
 
 	function toggleSwitch() {
 		setToggle(!toggle);
 	}
 
 
-
 	return (
 		<Center>
-			<TableContainer w="60vw" p="50px">
-				<Table variant='unstyled'>
+			
+			<TableContainer w="35vw" >
+				<Table variant='simple' color={theme.colors.forest}>
 					<Thead>
-						<Tr borderBottom="2px" w="90%">
-							<Th textTransform="lowercase" fontSize="2xl">{category}</Th>
-							<Th textTransform="lowercase" fontSize="2xl">{statName}</Th>
-							<Th pr="0px">
-								<HStack justifyContent="flex-end">
+					
+						<Tr borderBottom="2px" color={theme.colors.forest}>
+							<Th  fontSize="xl">{category}</Th>
+							<Th fontSize="xl" isNumeric>{statName}
+								<HStack justifyContent="flex-end" marginTop={"4%"}>
 									<BsPercent size="18px" color={theme.colors.forest}/>
-									<Switch size='md' onChange={toggleSwitch}/>
+									<Switch size='md' onChange={toggleSwitch} colorScheme={theme.colors.forest}/>
 									<TiSortNumerically size="20px" color={theme.colors.forest}/>
 								</HStack>
 							</Th>
 						</Tr>
+						
 					</Thead>
 					<Tbody>
-				
+
 						{items.sort((o1, o2) => toggle ? (o1.frequency_growth<o2.frequency_growth ? 1 : -1) : ( o1.search_count<o2.search_count ? 1:-1)).map((item, index) => {
 							return (<Tr key={index}>
-								<Td fontSize="3xl" p="20px">{`${index + 1}. ${item.word}`}</Td>
+								<Td fontSize="xl" >{`${index + 1}. ${item.word}`}</Td>
 								{ toggle ? 
-									<Td fontSize="3xl" p="20px">{item.frequency_growth}</Td>:
-									<Td fontSize="3xl" p="20px">{item.search_count}</Td> 
+									<Td fontSize="xl"  isNumeric>{item.frequency_growth}</Td>:
+									<Td fontSize="xl" isNumeric>{item.search_count}</Td> 
 								}
 							</Tr>);})}
 					</Tbody>
