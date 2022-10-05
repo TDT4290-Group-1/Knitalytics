a1423c7c6f60adc39e288ca3d4c727c7ac31aa0e
Jens Waage <waage.jens@gmail.com>
2022-10-05 19:05:18 +0200

    Implement retrieving data from Google Trends

--- backend/api/GoogleTrendsDataCollector.py
+++ backend/api/GoogleTrendsDataCollector.py
@@ -3,7 +3,6 @@ import io
 from typing import List
 from models.trending_word import TrendingWord
 from api.DataCollectorInterface import DataCollector
-from pandas import DataFrame
 
 from pytrends.request import TrendReq
 
@@ -12,37 +11,34 @@ America dreams;5.4;1000
 New knit sweater;3.4;100
 Blabla;6.4;10"""
 
-
+KNITTING_TOPIC = "/m/047fr" # google specific encoding of "Knitting" topic
 
 class GoogleTrendsDataCollector(DataCollector):
 
-    KNITTING_TOPIC = "/m/047fr" # google specific encoding of "Knitting" topic
 
     def __init__(self, host_language="en-US", tz=120) -> None:
         self.pytrends_client = TrendReq(host_language, tz)
 
 
     #Method use to collect raw data of trending words. Returns a pandas DataFrame of the raw data.
-    def __collect_trending_word_data__(self, data: str) -> DataFrame:
-        dataframe = pd.read_csv(io.StringIO(data), sep=";")
+    def __collect_trending_word_data__(self, geo="NO", timeframe="now 1-d") -> pd.DataFrame:
+        kw_list = [KNITTING_TOPIC]
+        self.pytrends_client.build_payload(kw_list, geo=geo, timeframe=timeframe)
+        response = self.pytrends_client.related_queries()
 
-        return dataframe
+        rising = response[KNITTING_TOPIC]["rising"]
+        top = response[KNITTING_TOPIC]["top"]
+        return pd.concat((rising, top))
 
     #Method used to process the raw data of trending words. Returns a list of TrendingWord objects from the given data frames.
-    def __process_trending_word_data__(self, data_frame: DataFrame) -> List[TrendingWord]:
-        trending_words: List[TrendingWord] = []
-
-        for row in data_frame.itertuples():
-            trending_words.append(
-                TrendingWord(row.word, row.frequency_growth, row.search_count)
-            )
-
+    def __process_trending_word_data__(self, data_frame: pd.DataFrame) -> List[TrendingWord]:
+        trending_words = ["word1", "word2"]
         return trending_words
 
     #Method used by the endpoint to get the trending words. Returns a list of TrendingWord objects.
     def get_trending_words(self) -> List[TrendingWord]:
 
         return self.__process_trending_word_data__(
-            self.__collect_trending_word_data__(raw_data)
+            self.__collect_trending_word_data__()
         )
 
