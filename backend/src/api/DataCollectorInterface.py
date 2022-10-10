34865aa70ecca80944fadd1ca48c67da8c3da62c
kavusiks <kavusikan98@hotmail.com>
2022-10-10 15:54:45 +0200

    (#30) code formatting

--- backend/src/api/DataCollectorInterface.py
+++ backend/src/api/DataCollectorInterface.py
@@ -2,7 +2,12 @@ from typing import List
 
 from models.trending_word import TrendingWord
 
-COLUMN_NAMES = {"word": "word", "frequency_growth": "frequency_growth", "search_count": "search_count"}
+COLUMN_NAMES = {
+    "word": "word",
+    "frequency_growth": "frequency_growth",
+    "search_count": "search_count",
+}
+
 
 class DataCollector:
     # Interface fors data collectors. All data collectors must implement this interface.
--- backend/src/api/GoogleTrendsDataCollector.py
+++ backend/src/api/GoogleTrendsDataCollector.py
@@ -3,30 +3,34 @@ from api.DataCollectorInterface import DataCollector, COLUMN_NAMES
 
 from pytrends.request import TrendReq
 
-KNITTING_TOPIC = "/m/047fr" # google specific encoding of "Knitting" topic
-COLUMN_MAPPER = {"query": COLUMN_NAMES["word"], "value": None} # mapper used to rename columns to standard values
+KNITTING_TOPIC = "/m/047fr"  # google specific encoding of "Knitting" topic
+COLUMN_MAPPER = {
+    "query": COLUMN_NAMES["word"],
+    "value": None,
+}  # mapper used to rename columns to standard values
+
 
 class GoogleTrendsDataCollector(DataCollector):
-
     def __init__(self, host_language="en-US", tz=120) -> None:
         self.pytrends_client = TrendReq(host_language, tz)
 
+    def __collect_trending_word_data__(
+        self, metric="frequency_growth", geo="NO", timeframe="now 1-d"
+    ) -> pd.DataFrame:
+        """
+        Collect top words on Google Trends according to a metric.
 
-    def __collect_trending_word_data__(self, metric="frequency_growth", geo="NO", timeframe="now 1-d") -> pd.DataFrame:
-        '''
-            Collect top words on Google Trends according to a metric.
+        Args:
+            metric : str, default 'frequency_growth'
+                Ranking metric of words. One of 'frequency_growth' or 'search_count'.
+            geo : str, default 'NO'
+                Geographical region to return searches for.
+            timeframe: str, default 'now 1-d'
+                Timeframe to return searches for. 'now #-d' represents last # days. Only supports 1 or 7 days.
 
-            Args:
-                metric : str, default 'frequency_growth'
-                    Ranking metric of words. One of 'frequency_growth' or 'search_count'.
-                geo : str, default 'NO'
-                    Geographical region to return searches for.
-                timeframe: str, default 'now 1-d'
-                    Timeframe to return searches for. 'now #-d' represents last # days. Only supports 1 or 7 days.
-            
-            Returns:
-                pandas.DataFrame
-        '''
+        Returns:
+            pandas.DataFrame
+        """
         kw_list = [KNITTING_TOPIC]
         self.pytrends_client.build_payload(kw_list, geo=geo, timeframe=timeframe)
         response = self.pytrends_client.related_queries()
@@ -35,13 +39,13 @@ class GoogleTrendsDataCollector(DataCollector):
             response = response[KNITTING_TOPIC]["rising"]
         elif metric == "search_count":
             response = response[KNITTING_TOPIC]["top"]
-        
+
         COLUMN_MAPPER["value"] = COLUMN_NAMES[metric]
 
         return response.rename(COLUMN_MAPPER, axis=1)
 
-    #Method used to process the raw data of trending words. Returns a list of TrendingWord objects from the given data frames.
-    def __process_trending_word_data__(self, data_frame: pd.DataFrame) -> pd.DataFrame: 
+    # Method used to process the raw data of trending words. Returns a list of TrendingWord objects from the given data frames.
+    def __process_trending_word_data__(self, data_frame: pd.DataFrame) -> pd.DataFrame:
         processed_data = data_frame.copy()
         return processed_data
 
@@ -51,4 +55,3 @@ class GoogleTrendsDataCollector(DataCollector):
         return self.__process_trending_word_data__(
             self.__collect_trending_word_data__()
         )
-
