ed99cba41d46069a7ea5dc7ea4a80551a65ecd48
Beate Simonsen <bmsimons@stud.ntnu.no>
2022-10-10 14:21:44 +0200

    (#63) WIP: Add connection with local storage

--- frontend/src/components/Settings/SettingsBox.tsx
+++ frontend/src/components/Settings/SettingsBox.tsx
@@ -1,22 +1,57 @@
-import { Flex, Text, SimpleGrid, Center, VStack } from "@chakra-ui/react";
+import { Text, SimpleGrid, Center, VStack, Input, HStack, Button } from "@chakra-ui/react";
+import { getListLocalStorage, setLocalStorageList } from "api/localStorage";
 import HashtagBox from "./HashtagBox";
+import {
+	AiOutlineSend,
+} from "react-icons/ai";
+import { useEffect, useRef, useState } from "react";
 
 interface SettingsBoxProps {
     title: string,
-    storagePath?: string,
-    
+    storagePath: string,
 }
 const SettingsBox = ({title, storagePath} : SettingsBoxProps) => {
+
+	const [input, setInput] = useState("");
+	const [buttonPress, setButtonPress] = useState(false);
+	const [listFromStorage, setListFromStorage] = useState(getListLocalStorage(storagePath).split(","));
+
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => setInput(event.target.value);
+	//const handleButtonPress = (event: React.MouseEventHandler<HTMLButtonElement, MouseEvent>) => setButtonPress(event.target.checked);
+	const handleButtonPress: React.MouseEventHandler = () => {setButtonPress(buttonPress => !buttonPress);};
+	const initialRender = useRef(true);
+
+	useEffect(() => {
+		console.log("hellooo from useeffect");
+		if (initialRender.current) {
+			initialRender.current = false;
+		} else {
+			const tempList = listFromStorage;
+			tempList.push(input);
+			setListFromStorage(tempList);
+			setLocalStorageList(listFromStorage.toString(), storagePath);
+		}
+	},[buttonPress]);
+
+	console.log("lsit from storage");
+	console.log(listFromStorage);
+
 	return (
 		<VStack>
 			<Center borderBottom="black">
 				<Text marginBottom={"10%"} fontSize={"2xl"}  color="forest">{title}</Text>
 			</Center>
 			<SimpleGrid minChildWidth="100px" spacing={10} w="100%">
-				<HashtagBox name="yarn"></HashtagBox>
-				<HashtagBox name="knitt"></HashtagBox>
-				<HashtagBox name="strikk"></HashtagBox>
+				{listFromStorage && listFromStorage.map((word: string, index: number) => (
+					<HashtagBox key={index} name={word}></HashtagBox>
+				))}
 			</SimpleGrid>
+			<HStack maxWidth="300px">
+				<Input placeholder='Add new word...' size='sm' value={input} onChange={handleInputChange}/>
+				<Button variant='ghost' onClick={handleButtonPress}>
+					<AiOutlineSend width="50px"></AiOutlineSend>
+				</Button>
+			</HStack>
 		</VStack>
 	);
 };
--- frontend/src/pages/settings/Settings.tsx
+++ frontend/src/pages/settings/Settings.tsx
@@ -8,9 +8,8 @@ const Settings = () => {
 				<Text fontSize={"6xl"} marginBottom={"6%"} color="forest">Settings</Text>
 			</Center>
 			<SimpleGrid minChildWidth='350px' spacingY='50px' spacingX="0px">
-				<SettingsBox title="Filtered out words"></SettingsBox>
+				<SettingsBox title="Filtered out words" storagePath="filteredOutWords"></SettingsBox>
 			</SimpleGrid>
-
 		</>
 	);
 };
