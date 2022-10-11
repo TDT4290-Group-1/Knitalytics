eaf9ca9949eed7e8c9bc2629efcc526e9ebc1cc5
Victoria Hopso <victoria.hopso@gmail.com>
2022-10-11 15:20:37 +0200

    (#88) clean up dead code

--- frontend/src/pages/instagramWatch/InstagramWatchPage.tsx
+++ frontend/src/pages/instagramWatch/InstagramWatchPage.tsx
@@ -1,9 +1,4 @@
-// import { ChevronLeftIcon } from "@chakra-ui/icons";
-// import {
-// 	IconButton,
-// } from "@chakra-ui/react";
 import InstagramPosts from "components/InstagramPost";
-// import { useNavigate } from "react-router-dom";
 
 
 const InstagramWatchPage = () => {
@@ -12,16 +7,9 @@ const InstagramWatchPage = () => {
      * TODO: replace urls with API data
      */
 	const urls: string[] = ["https://www.instagram.com/p/CjkVLbwK9VI/","https://www.instagram.com/p/CjkVLbwK9VI/","https://www.instagram.com/p/CjkVLbwK9VI/","https://www.instagram.com/p/CjkVLbwK9VI/","https://www.instagram.com/p/CjkVLbwK9VI/","https://www.instagram.com/p/CjkVLbwK9VI/","https://www.instagram.com/p/CjkVLbwK9VI/","https://www.instagram.com/p/CjkVLbwK9VI/","https://www.instagram.com/p/CjkVLbwK9VI/"];
-	// const navigate = useNavigate();
 
 	return(
-		<>
-			{/* <IconButton aria-label={"Back"} icon={<ChevronLeftIcon />} onClick={() => navigate("/")}>
-			</IconButton> */}
-			{/* kanskje fjern iconbutton */}
-			<InstagramPosts URLs={urls} heading={"Instagram posts from users you are following"}></InstagramPosts>
-
-		</>
+		<InstagramPosts URLs={urls} heading={"Instagram posts from users you are following"}></InstagramPosts>
 	);
 };
 
