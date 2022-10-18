4bf00c08afd7e03ee597b1c591ccaef7c0901419
Jonathan Helgesen <70104502+Jonathanhelgesen@users.noreply.github.com>
2022-10-18 08:54:08 +0200

    Render a box for each hashtag in followed hashtags

--- frontend/src/pages/hashtags/HashtagsPage.tsx
+++ frontend/src/pages/hashtags/HashtagsPage.tsx
@@ -1,35 +1,38 @@
 import { Box, Grid, GridItem, VStack } from "@chakra-ui/react";
 import InstagramPosts from "components/InstagramPost";
+import { getListLocalStorage} from "api/localStorage";
+import { useState } from "react";
 
 
-const InstagramWatchPage = () => {
+const HashtagsPage = () => {
 
 	/**
      * TODO: replace urls with API data
      */
 	const urls: string[] = ["https://www.instagram.com/p/CjkVLbwK9VI/","https://www.instagram.com/p/CjkVLbwK9VI/","https://www.instagram.com/p/CjkVLbwK9VI/","https://www.instagram.com/p/CjkVLbwK9VI/","https://www.instagram.com/p/CjkVLbwK9VI/","https://www.instagram.com/p/CjkVLbwK9VI/"];
+	const [hashtags] = useState(getListLocalStorage("followedHashtags").split(",").filter(element => {
+		return element !== "";}));
 
 	return(
 		<>
 			<Grid gap={4}>
-				<GridItem colSpan={1} bg='hovergreen' padding={"10px"} rounded={"lg"} paddingBottom={"50px"}>
-					<Box >
-						<VStack>
-							<InstagramPosts URLs={urls} heading={"Instagram posts with hashtag marius"}></InstagramPosts>
-						</VStack>
-					</Box>
-				</GridItem>
+				
+				{
+					hashtags.map((hashtag) => {
+						return(
+							<GridItem key={hashtag} colSpan={1} bg='hovergreen' padding={"30px"} rounded={"lg"} paddingBottom={"50px"}>
+								<Box >
+									<VStack>
+										<InstagramPosts URLs={urls} heading={hashtag}></InstagramPosts>
+									</VStack>
+								</Box>
+							</GridItem>);
+					})
+				}
 
-				<GridItem colSpan={1} bg='hovergreen' padding={"10px"} rounded={"lg"} paddingBottom={"30px"}>
-					<Box >
-						<VStack>
-							<InstagramPosts URLs={urls} heading={"Instagram posts with hashtag genser"}></InstagramPosts>
-						</VStack>
-					</Box>
-				</GridItem>
 			</Grid>
 		</>
 	);
 };
 
-export default InstagramWatchPage;
+export default HashtagsPage;
