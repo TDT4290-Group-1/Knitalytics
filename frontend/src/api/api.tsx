9efff3db9f935a2bf1a044b65ec2200ff8ae2ebb
Jens Waage <waage.jens@gmail.com>
2022-10-18 11:26:15 +0200

    Update frontend API function to no longer accept metric parameter

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -1,7 +1,6 @@
 
-import {TrendingWord} from "../../models/trendingword";
+import { TrendingWord } from "../../models/trendingword";
 import axios from "axios";
-import { TredningWordsMetric } from "utils/trendingWordsMetric";
 import { getListLocalStorage } from "api/localStorage";
 
 const client = axios.create({ baseURL: "http://127.0.0.1:5000/" });
@@ -10,41 +9,41 @@ const client = axios.create({ baseURL: "http://127.0.0.1:5000/" });
  */
 class API {
 
- 
+
 	/**
 	 * @param metric 'frequency_growth' or 'search_count'. Used to show the most searched words or the fastest growing words.
 	 * @param searchTerm Optional search term to search for. If empty, the default search term is used.
-     * @returns a JSON list of trening words with the gived metric value
-     */
-	async getAllTrendingWords(metric:TredningWordsMetric, searchTerm?:string):Promise<TrendingWord[]> {
-		const response = await client.get(`/api/v1/trends/?metric=${metric}${searchTerm ? "&search_term=" + searchTerm : ""}`);
+	 * @returns a JSON list of trening words with the gived metric value
+	 */
+	async getAllTrendingWords(searchTerm?: string): Promise<TrendingWord[]> {
+		const response = await client.get(`/api/v1/trends?${searchTerm ? "&search_term=" + searchTerm : ""}`);
 		return response.data;
 	}
 
 	/**
 	 * @param searchTerm The given search term to get interest over time for.
-     * @returns a JSON list of with date and relative interest value.
-     */
-	async getInteresOvertimeForSearchTerm(searchTerm:string):Promise<TrendingWord[]> {
+	 * @returns a JSON list of with date and relative interest value.
+	 */
+	async getInteresOvertimeForSearchTerm(searchTerm: string): Promise<TrendingWord[]> {
 		const response = await client.get(`/api/v1/interest_over_time/?search_term=${searchTerm}`);
 		return response.data;
 	}
 
-	async getAllRelatedHashtags(query: string):Promise<string[]> {
+	async getAllRelatedHashtags(query: string): Promise<string[]> {
 		const filteredOutWords = getListLocalStorage("filteredOutWords");
 		//filteredOutWords format: "word, word1, word2". String with comma between each word
 		const response = await client.get("/api/v1/relatedHashtags", { params: { query: query, filteredOutWords: filteredOutWords } });
 		return response.data;
 	}
 
-	async getAllRelatedPostURLS(query: string):Promise<string[]> {
+	async getAllRelatedPostURLS(query: string): Promise<string[]> {
 		const response = await client.get("/api/v1/relatedPostURLS", { params: { query: query } });
 		return response.data;
 	}
-        
+
 }
 
 export default new API();
-  
 
-  
+
+
