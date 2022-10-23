257ae6302e073398bf179ae27eaecaf40a4c7c1b
Jens Waage <waage.jens@gmail.com>
2022-10-23 17:21:57 +0200

    Add routing context to Homepage test

--- frontend/src/__tests__/HomePage.test.tsx
+++ frontend/src/__tests__/HomePage.test.tsx
@@ -3,9 +3,14 @@ import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
 
 import HomePage from "pages/home/";
+import { BrowserRouter } from "react-router-dom";
 
 test("Renders Words link", () => {
-	render(<HomePage />);
+	render(
+		<BrowserRouter>
+			<HomePage />
+		</BrowserRouter>
+	);
 	const linkElement = screen.getAllByText(/loading/i);
 	expect(linkElement);
 });
