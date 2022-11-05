3fd9cf34f9dca37f2685cafc1e56e1a873702f21
Lars Skifjeld <larssski@gmail.com>
2022-11-05 17:17:26 +0100

    (#144) add login page

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -7,12 +7,14 @@ import InstagramUsersPage from "pages/instagramUsers";
 import Settings from "pages/settings";
 import HashtagsPage from "pages/hashtags/HashtagsPage";
 import FashionBrandsPage from "pages/fashionBrands/FashionBrandsPage";
+import Login from "pages/login";
 
 const Router = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<HomePage />} />
 			<Route path="/settings" element={<Settings />} />
+			<Route path="/login" element={<Login />} />
 			<Route path="*" element={<NotFound />} />
 			<Route path="/GoogleDetails" element={<GoogleDetailsPage/>}/>
 			<Route path="/InstagramPosts" element={<InstagramUsersPage/>}/>
