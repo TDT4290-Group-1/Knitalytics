8162554fcbc8a0c5bc8279cbe9f09c87c0b60e8c
Lars Skifjeld <larssski@gmail.com>
2022-11-16 09:58:39 +0100

    Specify CORS origin

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ load_dotenv()
 def create_app():
 
     app = Flask(__name__)
-    CORS(app)
+    CORS(app, resources={r"/api/*": {"origins": "https://knitalytics.web.app/"}})
     app.config["CORS_HEADERS"] = "Content-Type"
 
     def add_dataframe_from_collector(
